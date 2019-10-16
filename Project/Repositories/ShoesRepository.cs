ea4b10198442b5d8447a9ad254b3f25604d34343
Josiah Aubin <josiahaubin@gmail.com>
2019-10-16 09:48:19 -0600

    Before Morning Review

--- Project/Repositories/ShoesRepository.cs
+++ Project/Repositories/ShoesRepository.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Collections.Generic;
+using simpleshoe.Models;
+
 namespace simpleshoe.Project.Repositories
 {
-    public class ShoesRepository
+  public class ShoesRepository
+  {
+    internal IEnumerable<Shoe> Get()
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal Shoe Get(string id)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
--- Project/Services/ShoesService.cs
+++ Project/Services/ShoesService.cs
@@ -1,19 +1,29 @@
 using System;
 using System.Collections.Generic;
 using simpleshoe.Models;
+using simpleshoe.Project.Repositories;
 
 namespace simpleshoe.Project.Services
 {
   public class ShoesService
   {
+    private readonly ShoesRepository _repo;
+
+    public ShoesService(ShoesRepository repo)
+    {
+      _repo = repo;
+    }
+
     public IEnumerable<Shoe> Get()
     {
-      throw new NotImplementedException();
+      return _repo.Get();
     }
 
     public Shoe Get(string id)
     {
-      throw new NotImplementedException();
+      Shoe exists = _repo.Get(id);
+      if (exists == null) { throw new Exception("Invalid ID"); }
+      return exists;
     }
 
     public Shoe Create(Shoe newShoe)
